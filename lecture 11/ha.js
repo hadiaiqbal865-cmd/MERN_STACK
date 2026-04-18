83698ea3459dd1033a4b69c6f9d33d562a948a3b
hadiaiqbal865-cmd <hadiaiqbal865@gmail.com>
2026-04-18 03:21:24 -0700

    Initial upload

---
+++ lecture 11/ha.js
@@ -0,0 +1,17 @@
+// ES5: Echma Script
+// ES6: Echma Script
+// Values and Variables
+// Variables: Memory Containers, (which stores Data or Values)
+// x = 10;
+// y = 34;
+// z = x + y;
+// z = 44;
+let x = 10;
+let y = 23;
+let z = x + y;
+console.log(z);
+let firstName = "Jonas";
+let lastName = "Bob";
+console.log(firstName, lastName);
+console.log("firstName", "lastName", z, x, y);
+console.log("a");

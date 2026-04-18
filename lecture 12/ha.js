2b1dcace1722a1509e32e0c5bd075e25e625087e
hadiaiqbal865-cmd <hadiaiqbal865@gmail.com>
2026-04-18 03:24:25 -0700

    Add files via upload

---
+++ lecture 12/ha.js
@@ -0,0 +1,41 @@
+let firstName = "jonas";
+let lastName = "Bob";
+console.log(firstName , lastName);
+// Rules to name a variable
+// => you cannot use number at begining
+// let 2firstName(not allowed), let firstName223(allowed);
+// let first-Name(not allowed), first_Name(allowed)
+// let first#Name(not allowed)
+// let FirstName,(Capital letter(not generate errors but avoid to use it))
+// => Cannot use builtin words(pre-defined)
+// console, if,for,function
+// let first Name;
+// let my name = "Hadia";
+ let myName34 = 34;
+ console.log(myName34);
+ // Variable Declaration
+ let y;
+ // Variable Initilization
+ let z = 67;
+ // 1. Declear variables called 'country','continent' and 'population' and assign their values according to ypur own country(population in millions)
+ // 2. Log their values to the console
+ let country = "pakistan";
+ let continent = "Asia";
+ let population = 344;
+ console.log(country,continent,population);
+ // Data Types
+ // primptive and non-primptive
+ //primptive:
+ // There are seven primptive data types in javascript
+ // 1. Number(Decimal and Non-Decimal)
+ // let x = 23.56; let y = 45;
+ // 2. String(Characters Form)
+ // let firstName ="Hadia";
+ // 3. Boolean(True and False)
+ // e.g let f = 34>20=> True 
+ // e.g let k =34>45=> False
+ // 4. Undefined(Empty Value)
+ // e.g let n;
+ // 5. Null(having not a worth)
+ // let a = 45;
+ 

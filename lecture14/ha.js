0f7438e8619b1961da13b6515e429c5dfc78d3d6
hadiaiqbal865-cmd <hadiaiqbal865@gmail.com>
2026-05-06 09:23:07 -0700

    Add files via upload

---
+++ lecture14/ha.js
@@ -0,0 +1,37 @@
+// Opeartors in Js
+
+ // 1. Arithmetic Operators:(+,_,*,/,++,--,%)
+ let x = 5;
+ let y = 8;
+ console.log("x + y = ", x + y);
+ console.log("x - y = ", x - y);
+ console.log("x * y = ", x * y);
+ console.log("x / y = ", x / y);
+ console.log("x % y = ", x % y);
+x++; // x = x + 1;
+console.log(x);
+y--; // y = y + 1;
+console.log(y);
+
+// 2. Assignment Operators:(=,+=,-=,*=,/=)
+let a = 40;
+a += 5; // a = a + 5;
+console.log(a);
+a -= 10; // a = a - 10;
+console.log(a);
+a *= 2; // a = a * 2;
+console.log(a);
+a /= 5; // a = a / 5;
+console.log(a);
+
+// 3. Comparison Operators: (==, ===, <=, <, >, >=)
+let d = 67;
+let e = 67;
+console.log(e == d); // loose equality operator
+console.log(d === e); // strict equality operator
+console.log(d < e);
+console.log(d <= e);
+console.log(d > e);
+console.log(d >= e);
+
+// 4. logical operators: (&&, !, ||, NAND, NOR)
